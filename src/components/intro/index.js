8c76dfc1b4a1b8ba292ca5a7cd05001e4d280da4
Jason Geiger <448523+jasonjgeiger@users.noreply.github.com>
2020-05-03 12:16:58 -0700

    mobile maps & phone

--- src/components/intro/index.js
+++ src/components/intro/index.js
@@ -39,10 +39,10 @@ const Intro = () => (
                 <p className={Styles.body}>Stop in to see (and smell!) our full selection&nbsp;<br className={Styles.hideMdDown}/>of traditional Mexican pastries and bolillo&nbsp;<br className={Styles.hideMdDown}/>(Mexican white bread) baked fresh daily!</p>
                 <div className={Styles.section} id="#contact">
                   <div className={Styles.section}>
-                    <p className={Styles.heading}>{address.streetAddress}<br/>{address.addressLocality}, {address.addressRegion} {address.postalCode}</p>
+                    <p className={Styles.heading}><a href={`maps://?q=${address.streetAddress}${address.addressLocality}, ${address.addressRegion} ${address.postalCode}`}>{address.streetAddress}<br/>{address.addressLocality}, {address.addressRegion} {address.postalCode}</a></p>
                   </div>
                   <div className={Styles.section}>
-                    <p className={Styles.heading}>Call to order<br/>P {telephone}<br/>F {fax}</p>
+                    <p className={Styles.heading}>Call to order<br/>P <a href={`tel:${telephone}`}>{telephone}</a><br/>F {fax}</p>
                   </div>
                 </div>
                 <script type="application/ld+json">
